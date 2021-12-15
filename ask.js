eb95a975d9b28a0897107672680474240d0fcf49
greggry <greggryw@protonmail.com>
2021-12-15 19:19:47 +0100

    exporting a class

--- ask.js
+++ ask.js
@@ -1,25 +1,32 @@
 const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+class Ask {
+  constructor(rlInterface) {
+    // create own interface
+    if (!rlInterface) {
+      this.rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+      return this; // escape
+    }
 
-// promisify rl.question()
-function prompt(questionString) {
-  return new Promise(resolve => {
-    rl.question(questionString, answer => {
-      resolve(answer);
+    this.rl = rlInterface;
+  }
+
+  // promisify rl.question()
+  ask(questionString) {
+    return new Promise(resolve => {
+      this.rl.question(questionString, answer => {
+        resolve(answer);
+      });
     });
-  });
+  }
+
+  // has to be run to stop waiting for input
+  closeInterface() {
+    this.rl.close();
+  }
 }
 
-// has to be run to stop waiting for input
-function closePrompt() {
-  rl.close();
-}
-
-module.exports = {
-  prompt,
-  closePrompt,
-};
+module.exports = Ask;
